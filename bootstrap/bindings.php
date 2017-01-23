39c4a687954337777e6ccfb3e577bc2b365c3ebe
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-23 18:42:37 +0330

    ::class bindings.

--- bootstrap/bindings.php
+++ bootstrap/bindings.php
@@ -1,7 +1,7 @@
 <?php
 
-$container->bind('http', 'GuzzleHttp\Client');
+$container->bind('http', GuzzleHttp\Client::class);
 
-$container->singleton('identityPool', 'Profounder\Services\IdentityPool');
+$container->singleton('identityPool', Profounder\Services\IdentityPool::class);
 
-$container->singleton('Illuminate\Contracts\Debug\ExceptionHandler', 'Profounder\Exceptions\Handler');
+$container->singleton('Illuminate\Contracts\Debug\ExceptionHandler', Profounder\Exceptions\Handler::class);
