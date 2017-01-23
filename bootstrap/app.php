676bdc910deb7c3f3e5c62a1d11915c0e8dfbd7c
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-23 15:08:54 +0330

    Conditional command registering.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,19 +7,23 @@ use Symfony\Component\Console\Application;
 $container   = require_once 'container.php';
 $application = new Application($container->config->get('name'), $container->config->get('version'));
 
-$application->addCommands(array_map(
-    function ($command) use ($container) {
-        return new $command($container);
-    },
-    $container->config->get('commands.container-aware')
-));
+if ($commands = $container->config->get('commands.container-aware')) {
+    $application->addCommands(array_map(
+        function ($command) use ($container) {
+            return new $command($container);
+        },
+        $commands
+    ));
+}
 
-$application->addCommands(array_map(
-    function ($command) {
-        return new $command;
-    },
-    $container->config->get('commands.non-container-aware')
-));
+if ($commands = $container->config->get('commands.non-container-aware')) {
+    $application->addCommands(array_map(
+        function ($command) {
+            return new $command;
+        },
+        $commands
+    ));
+}
 
 set_time_limit(0);
 date_default_timezone_set($container->config->get('timezone'));
