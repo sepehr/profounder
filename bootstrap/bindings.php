f44f4e88af683ffc58c5101128be5ab8a534f88c
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-23 20:03:41 +0330

    No bindings for Illuminate\Contracts\Debug\ExceptionHandler

--- bootstrap/bindings.php
+++ bootstrap/bindings.php
@@ -1,7 +1,5 @@
 <?php
 
-$container->bind('http', GuzzleHttp\Client::class);
-
 $container->singleton('identityPool', Profounder\Services\IdentityPool::class);
 
-$container->singleton('Illuminate\Contracts\Debug\ExceptionHandler', Profounder\Exceptions\Handler::class);
+$container->bind('http', GuzzleHttp\Client::class);
